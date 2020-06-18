046187c0265a89895cc52af50736cfcf10216596
JssRM48 <jss211408@gmail.com>
2020-06-17 23:34:38 -0500

    avance historia

---
+++ formato_historia_clinica.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Historia Clinica</title>
+	<link rel="stylesheet" type="text/css" href="css/css_pacientes.css">
+	<link rel="stylesheet" type="text/css" href="css/css_principal.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+</head>
+<body>
+	<!-- Cabecera principal -->
+	<header class="header">
+		<div class="container logo-per-container">
+			<a href="index.php" class="logo"><b>SMART NUTRITION</b></a>
+			<h7 class="perfil">Nombre del licenciado</h7>
+		</div>
+	</header>
+
+	<!-- Contenido de la aplicacion -->
+	<main class="main">
+		<div class="container">
+			<h1>Historia Clinica</h1>
+		</div>
+		
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<div class="wrap-input100 validate-input bg1 rs1-wrap-input100">
+					
+				</div>
+				
+				<input type="submit" class="btn-buscar" name="buscar" value="Aceptar">
+				<input type="reset" class="btn-buscar" name="cancelar" value="Cancelar">
+				<button class="btn-nueva" name="buscar">
+					<a href="#">Imprimir</a>
+				</button>
+			</form>
+		</div>
+	</main>
+</body>
+</html>
