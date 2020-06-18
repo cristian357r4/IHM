02603a13dcd494501d5690fdc43b6f866db34bbc
cristian357r4 <cristian357r4@gmail.com>
2020-06-18 00:05:04 -0500

    Modifique el titulo de las paginas ya dice Smart nutrition

--- plan_alimentacion.php
+++ plan_alimentacion.php
@@ -4,7 +4,7 @@ session_start();
 <!doctype html>
 <html lang="es-mx">
 <head>
-    <title>Bootstrap Example</title>
+    <title>Smart Nutrition</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
