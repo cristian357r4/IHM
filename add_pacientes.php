c23ebe9321f6bac4f52a99bf6baca77197f74089
JssRM48 <jss211408@gmail.com>
2020-06-21 18:03:55 -0500

    ingresar.php

---
+++ add_pacientes.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		error_reporting(E_ALL);
+		ini_set('display_errors', '1');
+
+    	include("conexion.php");
+
+    	$pnombre = $_POST['nombre'];
+    	$pedad = $_POST['edad'];
+    	$psexo = $_POST['sexo'];
+    	$preligion = $_POST['religion']; 
+    	$pestadocivil = $_POST['estadocivil'];
+    	$pocupacion = $_POST['ocupacion'];
+    	$pfechadenacimiento = $_POST['fecha'];
+    	$ptelefono = $_POST['telefono'];
+    	$pemail = $_POST['correo'];
+    	$ppeso = $_POST['peso'];
+    	$pestatura = $_POST['estatura'];
+
+    	$query = "INSERT INTO PACIENTE (NOMBRE,EDAD,SEXO,RELIGION,ESTADOCIVIL,OCUPACION,FECHADENACIMIENTO,TELEFONO,CORREO,PESO,ESTATURA) VALUES ('$pnombre','$pedad','$psexo','$preligion','$pestadocivil','$pocupacion','$pfechadenacimiento','$ptelefono','$pemail','$ppeso','$pestatura')";
+
+    	$result = mysqli_query($conn,$query);
+
+    	if(mysqli_num_rows($result) > 0){
+    		mysqli_close($conn);
+    		echo "ingreso correcto del paciente";
+    	}else{
+    		mysqli_close($conn);
+    		header('Location: ac_pacientes.php')
+    		echo "algo esta mal, no se logro el registro";
+    	}
+	?>
+</body>
+</html>
