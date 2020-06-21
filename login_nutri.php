672fea86dc86a1194f1cbf7ef83e55e448f4067e
JssRM48 <jss211408@gmail.com>
2020-06-20 22:13:50 -0500

    4567

--- login_nutri.php
+++ login_nutri.php
@@ -13,19 +13,17 @@
     	$nuser = $_POST['user'];
     	$npass = $_POST['pass'];
 
-    	$query = "SELECT USUARIO, CONTRASENYA FROM ADMINISTRADOR WHERE USUARIO = '$nuser' AND CONSTRASENYA = '$npass'";
+    	$query = "SELECT USUARIO, CONTRASENYA FROM ADMINISTRADOR WHERE USUARIO = '$nuser' AND CONTRASENYA = '$npass'";
     	echo $query;
 
     	$result = mysqli_query($conn,$query);
 
     	if (mysqli_num_rows($result) > 0) {
-  			echo("entra");
   			mysqli_close($conn);
-  			//header('Location: index.php');
+  			header('Location: index.php');
 		}else{
-			echo "no entra";
 			mysqli_close($conn);
-			//header('Location: login_nutriologos.php');
+			header('Location: login_nutriologos.php');
 		}
 
 	?>
