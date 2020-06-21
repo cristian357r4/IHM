3558e3d3b70a47796e85c221b71a8cebe5af40c4
JssRM48 <jss211408@gmail.com>
2020-06-20 22:23:29 -0500

    asdfghj

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("107.180.41.26","agjesus","123456","nutritionsmart");
+	$conn = mysqli_connect("107.180.41.126","agjesus","123456","nutritionsmart");
 
 	if($conn -> connect_error)
 		die($conn -> connect_error);
