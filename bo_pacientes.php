536ac29079516bb3de19491e587bac642a3de798
JssRM48 <jss211408@gmail.com>
2020-06-23 20:58:44 -0500

    borrar

--- bo_pacientes.php
+++ bo_pacientes.php
@@ -76,112 +76,46 @@
 			<form class="contact100-form validate-form">
 				<h5>Bajas paciente</h5>
 					<table class="tabla-consulta">
-						<tr class="tabla-tr">
-							<th class="tabla-td-th">Nombre</th>
-							<th class="tabla-td-th">Edad</th>
-							<th class="tabla-td-th">Sexo</th>
-							<th class="tabla-td-th">Religion</th>
-							<th class="tabla-td-th">Estado civil</th>
-							<th class="tabla-td-th">Ocupacion</th>
-							<th class="tabla-td-th">Fecha de nacimiento</th>
-							<th class="tabla-td-th">Telefono</th>
-							<th class="tabla-td-th">Email</th>
-							<th class="tabla-td-th">Peso</th>
-							<th class="tabla-td-th">Estatura</th>
-							<th class="tabla-td-th">
-								
-							</th>
-						</tr>
-						<tr class="tabla-tr">
-							<td class="tabla-td-th">Pedro Ramirez</td>
-							<td class="tabla-td-th">26</td>
-							<td class="tabla-td-th">M</td>
-							<td class="tabla-td-th">Ateo</td>
-							<td class="tabla-td-th">Soltero</td>
-							<td class="tabla-td-th">Estudiante</td>
-							<td class="tabla-td-th">1993-05-08</td>
-							<td class="tabla-td-th">7221689176</td>
-							<td class="tabla-td-th">js8@gmail.com</td>
-							<td class="tabla-td-th">97</td>
-							<td class="tabla-td-th">1.70</td>
-							<td class="tabla-td-th">
-								<button>
-									<b>Borrar</b>
-								</button>
-							</td>
-						</tr>
-						<tr class="tabla-tr">
-							<td class="tabla-td-th">Rosa Lopez</td>
-							<td class="tabla-td-th">16</td>
-							<td class="tabla-td-th">F</td>
-							<td class="tabla-td-th">Catolico</td>
-							<td class="tabla-td-th">Soltero</td>
-							<td class="tabla-td-th">Estudiante</td>
-							<td class="tabla-td-th">2004-11-08</td>
-							<td class="tabla-td-th">7221685548</td>
-							<td class="tabla-td-th">rl14@gmail.com</td>
-							<td class="tabla-td-th">56</td>
-							<td class="tabla-td-th">1.60</td>
-							<td class="tabla-td-th">
-								<button>
-									<b>Borrar</b>
-								</button>
-							</td>
-						</tr>
-						<tr class="tabla-tr">
-							<td class="tabla-td-th">Jose Ramos</td>
-							<td class="tabla-td-th">27</td>
-							<td class="tabla-td-th">M</td>
-							<td class="tabla-td-th">Testigo de Jehova</td>
-							<td class="tabla-td-th">Casado</td>
-							<td class="tabla-td-th">Obrero</td>
-							<td class="tabla-td-th">1992-07-05</td>
-							<td class="tabla-td-th">7225488776</td>
-							<td class="tabla-td-th">jr3@gmail.com</td>
-							<td class="tabla-td-th">85</td>
-							<td class="tabla-td-th">1.80</td>
-							<td class="tabla-td-th">
-								<button>
-									<b>Borrar</b>
-								</button>
-							</td>
-						</tr>
-						<tr class="tabla-tr">
-							<td class="tabla-td-th">Sofia Flores</td>
-							<td class="tabla-td-th">24</td>
-							<td class="tabla-td-th">F</td>
-							<td class="tabla-td-th">Catolica</td>
-							<td class="tabla-td-th">Casado</td>
-							<td class="tabla-td-th">Licenciada</td>
-							<td class="tabla-td-th">1995-06-07</td>
-							<td class="tabla-td-th">7227531245</td>
-							<td class="tabla-td-th">saphie@gmail.com</td>
-							<td class="tabla-td-th">55</td>
-							<td class="tabla-td-th">1.58</td>
-							<td class="tabla-td-th">
-								<button>
-									<b>Borrar</b>
-								</button>
-							</td>
-						</tr>
-						<tr class="tabla-tr">
-							<td class="tabla-td-th">Carolina Gonzalez</td>
-							<td class="tabla-td-th">27</td>
-							<td class="tabla-td-th">F</td>
-							<td class="tabla-td-th">Catolico</td>
-							<td class="tabla-td-th">Comprometida</td>
-							<td class="tabla-td-th">Licenciada</td>
-							<td class="tabla-td-th">1993-05-21</td>
-							<td class="tabla-td-th">7221686478</td>
-							<td class="tabla-td-th">carog@gmail.com</td>
-							<td class="tabla-td-th">55</td>
-							<td class="tabla-td-th">1.54</td>
-							<td class="tabla-td-th">
-								<button>
-									<b>Borrar</b>
-								</button>
-							</td>
-						</tr>
+						<thead>
+							<tr class="tabla-tr">
+								<th class="tabla-td-th" id="nombre">Nombre</th>
+								<th class="tabla-td-th" id="edad">Edad</th>
+								<th class="tabla-td-th" id="sexo">Sexo</th>
+								<th class="tabla-td-th" id="religion">Religion</th>
+								<th class="tabla-td-th" id="estadocivil">Estado civil</th>
+								<th class="tabla-td-th" id="ocupacion">Ocupacion</th>
+								<th class="tabla-td-th" id="fecha">Fecha de nacimiento</th>
+								<th class="tabla-td-th" id="telefono">Telefono</th>
+								<th class="tabla-td-th" id="correo">Email</th>
+								<th class="tabla-td-th" id="peso">Peso</th>
+								<th class="tabla-td-th" id="estatura">Estatura</th>
+								<th class="tabla-td-th">
+									Eliminar
+								</th>
+							</tr>	
+						</thead>
+						<tbody>
+							<?php while ($filas = mysqli_fetch_assoc($resultado))
+							?>
+							<tr class="tabla-tr">
+								<td class="tabla-td-th"><?php echo $filas['NOMBRE'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['EDAD'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['SEXO'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['RELIGION'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['ESTADOCIVIL'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['OCUPACION'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['FECHADENACIMIENTO'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['TELEFONO'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['CORREO'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['PESO'] ?></td>
+								<td class="tabla-td-th"><?php echo $filas['ESTATURA']?></td>
+								<td class="tabla-td-th">
+									<a href="">Eliminar</a>	
+								</td>
+							</tr>
+							<?php } ?>
+						</tbody>
+						
 					</table>
 			</form>
 		</div>
