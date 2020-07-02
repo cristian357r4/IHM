550ee3cf7bf709af1aea8c9de1957ada8ed01728
JssRM48 <jss211408@gmail.com>
2020-07-01 22:48:24 -0500

    qwerty 4

--- co_alimento.php
+++ co_alimento.php
@@ -12,7 +12,7 @@
 <body>
 	<?php 
 		include("conexion.php");
-		$querycereales = "SELECT ID,INGREDIENTE,CANTIDAD FROM TABLASALIMENTICION WHERE GRUPOALIMENTICIO = 'Cereales y tubérculos'";
+		$querycereales = "SELECT IDDETABLASDEALIMENTACION,INGREDIENTE,CANTIDAD FROM TABLASALIMENTICION WHERE GRUPOALIMENTICIO = 'Cereales y tubérculos'";
 		$resultadocereales = mysqli_query($conn,$querycereales);
 	?>
 	<!-- Cabecera principal -->
